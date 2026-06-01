434e323a199ba6216be8a8674d1b33f14e860a84
AdnanBaig101 <mirzaadnanbaig461@gmail.com>
2026-06-01 19:21:46 +0530

    Add BankAccount class with deposit and balance display

--- q1.kt
+++ q1.kt
@@ -1,5 +1,19 @@
-fun main() {
-    for(i in 1..10) {
-        println(i)
+class BankAccount {
+    private var balance: Double = 0.0
+
+    fun deposit(amount: Double) {
+        if (amount > 0) {
+            balance += amount
+        }
     }
+
+    fun showBalance() {
+        println("Balance: $balance")
+    }
+}
+
+fun main() {
+    val account = BankAccount()
+    account.deposit(1000.0)
+    account.showBalance()
 }
